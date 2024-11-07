5b2aa4643cb2ae4e5752265f63484966f586f9ff
Stepan Shapovalov <s.shapovalov@ispsystem.com>
2024-11-07 17:35:08 +0800

    more examples and notes

--- apps/slots-app/src/app/app.component.ts
+++ apps/slots-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SimpleComponentModule, SimpleComponentNoShadowModule, SimpleComponentDefaultSlotModule } from '@rockssh/slots-wrapper';
+import { NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [ RouterModule, SimpleComponentModule, SimpleComponentNoShadowModule, SimpleComponentDefaultSlotModule],
+  imports: [
+    RouterModule,
+    SimpleComponentModule,
+    SimpleComponentNoShadowModule,
+    SimpleComponentDefaultSlotModule,
+    NgIf,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
