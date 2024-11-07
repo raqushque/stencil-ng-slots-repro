520d4fdbf2916c544a545a30134e6fe191d157f6
Stepan Shapovalov <s.shapovalov@ispsystem.com>
2024-11-07 17:07:26 +0800

    fix style names

--- packages/components/src/components/simple-component-default-slot/simple-component-default-slot.tsx
+++ packages/components/src/components/simple-component-default-slot/simple-component-default-slot.tsx
@@ -8,21 +8,21 @@ import { Component, Host, h } from '@stencil/core';
 export class SimpleComponentDefaultSlot {
   render() {
     return (
-      <Host class="simple-component-no-shadow">
-        <div class="simple-component-no-shadow__description-container">
-          <span class="simple-component-no-shadow__description">
+      <Host class="simple-component-default-slot">
+        <div class="simple-component-default-slot__description-container">
+          <span class="simple-component-default-slot__description">
             This component does not use Shadow DOM and has default slot.
           </span>
         </div>
-        <p class="simple-component-no-shadow__slot-boundary-description">
+        <p class="simple-component-default-slot__slot-boundary-description">
           Content with slot="content" should be slotted after this paragraph...
         </p>
         <slot name="content"></slot>
-        <p class="simple-component-no-shadow__slot-boundary-description">
+        <p class="simple-component-default-slot__slot-boundary-description">
           ...and before this paragraph.
         </p>
-        <div class="simple-component-no-shadow__default-slot-container">
-          <p class="simple-component-no-shadow__slot-boundary-description">
+        <div class="simple-component-default-slot__default-slot-container">
+          <p class="simple-component-default-slot__slot-boundary-description">
             This is a default slot container
           </p>
           <slot></slot>
